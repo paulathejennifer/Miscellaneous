c75dbc318f86e4ece45a66863f68354aeb95be3a
paulathejennifer <paulathejennife@gmail.com>
2025-03-22 17:00:10 +0300

    table website and index.js incomplete for now

--- index.js
+++ index.js
@@ -1,99 +1,83 @@
-// let  text = "Hello World!";
-// let extractWord = text.split("")[1].replace("!", "");
-// console.log(extractWord);
+let String2 = "Jen";
+let String1 = String("Mlo");
+let first1 = String1[0];
+let first2 = String1.charAt(2);
+let first3 = String1[1];
 
-let text = "My name  is 'Jennifer'";
-let length = text.length;
-console.log(length);
 
-let namey = "Jennifer " + "Chichi";
-console.log(namey);
 
-let x =String ("James");
-let y = new String ("James");
-console.log(x===y);
+let text = " To School"
+let textUpper = text.toUpperCase();
+console.log(textUpper);
 
-let q = "I love coding";
-let char = q.charCodeAt(2);
-console.log(char);
+let q = "To campus"
+let qlower = q.toLowerCase();
+console.log(qlower);
 
-const brand = "W3schools";
-let letter = brand.at(-2);
-console.log(letter);
+let r ="I love cooking";
+let r2 = r.concat(" So much");
+console.log(r2);
 
-a = "I am a girl";
-let extractedWord = a[7];
-console.log(extractedWord);
+let s = "I am a girl";
+let s2 = s.indexOf('I');
+console.log(s2);
 
-let fruits = "Orange, Dates, Kiwi";
-let sliced = fruits.slice(8,13);
-console.log(sliced);
+let t = "Have you ever ate Chicken?"
+let t2 = t.slice(9, );
+console.log(t2);
 
-let sweet = "Apple, Banana, Kiwi";
-let part = sweet.substring(7,13);
-console.log(part);
+let u = " Wow Everyone, Good job!"
+console.log(u);
 
-let madam = "Jennifer";
-let officialize = madam.charAt(0).toUpperCase() + madam.slice(1);
-console.log(officialize);
+let m = ["Orange", "Mango", "Lemon", "Grapejuice"];
+let m2 = m.pop();
+console.log(m2);
 
-let text1 = "Hi";
-let text2 = "JavaScript";
-let text3 = text1.concat(" " , text2);
-console.log(text3);
+let arr = [{name: 'Jenny'} , {name: 'Alice'}]
+let meArr = arr.map(elem => elem.name);
+console.log(meArr);
 
-let intro = "Hello" + " " + "Everybody";
-console.log(intro);
+let arr2 = [{age: 17}, {age:19}]
+let ageArr = arr2.forEach(elem => console.log(elem));
 
-let boyfriend = " James ";
-let newCrush = boyfriend.trimEnd()
-console.log(newCrush);
 
-let luckyNumber = "X";
-let padded = luckyNumber.padEnd(8,"x");
-console.log(padded);
+let arr3 = [{grandma:'Alice'}, {grandma: 'Mary'}]
+let grannyArr = arr3.forEach(elem => console.log(elem));
 
-let myNumber = 5;
-let mypadText = myNumber.toString();
-let pading = mypadText.padEnd( 5,"3");
-console.log(pading);
+let arr4 =[{dream: 'MANSION'}, {dream: 'LAMBORGHINI'}]
+let dreamArr = arr4.forEach(elem => console.log(elem));
 
-let location = "Please do jennee where 'jennee' is jennee";
- let locationn =location.search("jennee")
-console.log(locationn);
+let filterNo = [1, 2, 4, 5, 7];
+let filter = filterNo.filter(elem => elem > 2)
+console.log(filter);
 
-let yearOfBirth = "2005";
-let YOB = yearOfBirth.match(/05/g);
-console.log(YOB);
+let mpesaBal = [2000, 345, 500, 340000];
+let filterBal = mpesaBal.some(elem => elem < 1000);
+console.log(filterBal);
 
-let mom = "pauline Wachira";
-let whereIsShe = mom.matchAll(/pau/gi);
-console.log(whereIsShe);
+let budget = [1000, 300, 700, 345];
+let filterBudg = budget.some(elem => elem >= 10000);
+console.log(filterBudg);
 
-let Me = "Paula The Jennifer";
-let findMe = Me.endsWith("Jennifer");
-console.log(findMe);
 
-let firstName = "Edith";
-let lastName = "Kandie";
-let program = "codehiveX";
-let welcome = `Let me take this opportunity to congratulate you ${firstName} ${lastName} for managing to join ${program} program!`
-console.log(welcome);
 
-let price = 100;
-let VAT = 0.25;
-let total = `Total: ${(price + VAT).toFixed(1)}`;
-console.log(total);
 
-let budget = 10000;
-let transactionCost = 2.25;
-let sum = `Total: ${(budget + transactionCost).toFixed(4)}`;
-console.log(sum);
 
-let shoppingCost = 50003;
-let items = 6;
-let productCost = `Total Money to be spent for stocking: ${(shoppingCost * items).toFixed(0)}`;
-console.log(productCost);
+
+
+
+
+
+
+
+
+// console.log(first3);
+// console.log(first2);
+// console.log(first1);
+// console.log(String1);
+// console.log(String2);
+
+
 
 
 
